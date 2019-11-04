747ad8b4affc085dc046131f016f970bc2f959c2
amazingyyc <amazingyyc@outlook.com>
2019-11-04 13:26:01 +0800

    optimize timeline

--- dadt/internal/timeline.cc
+++ dadt/internal/timeline.cc
@@ -2,18 +2,19 @@
 
 namespace dadt {
 
-TimeLine::TimeLine(const std::string &timeline_path): timeline_path_(timeline_path) {
-  // open file
-  writer_.open(timeline_path_, std::ios::out);
+TimeLine::TimeLine(const std::string &timeline_path)
+  : timeline_path_(timeline_path), initialized_(false), stoped_(false) {
+  writer_thread_ = std::thread(&TimeLine::writer_do_work, this);
 
-  ARGUMENT_CHECK(writer_.is_open(), "open file:" << timeline_path_ << " failed!");
-
-  writer_ << "[\n";
-  writer_.flush();
+  while (false == initialized_.load()) {
+    // just wait
+  }
 }
 
 TimeLine::~TimeLine() {
-  writer_.close();
+  stoped_ = true;
+
+  writer_thread_.join();
 }
 
 // get current microseconds
@@ -23,121 +24,191 @@ int64_t TimeLine::get_current_microseconds() {
   return duration_in_ms.count();
 }
 
-void TimeLine::write(const std::string &event_name, 
-                     const std::string &cat, 
-                     const std::string &ph, 
-                     const std::string &op_name, 
-                     int64_t ts) {
+void TimeLine::write(const TimeEvent &event) {
   writer_ << "{";
-  writer_ << "\"name\": \"" << event_name << "\"";
+  writer_ << "\"name\": \"" << event.name << "\"";
   writer_ << ",";
-  writer_ << "\"cat\": \"" << cat << "\"";
+  writer_ << "\"cat\": \"" << event.cat << "\"";
   writer_ << ",";
-  writer_ << "\"ph\": \"" << ph << "\"";
+  writer_ << "\"ph\": \"" << event.ph << "\"";
   writer_ << ",";
-  writer_ << "\"pid\": \"" << op_name << "\"";
+  writer_ << "\"pid\": \"" << event.pid << "\"";
   writer_ << ",";
-  writer_ << "\"ts\":" << ts;
+  writer_ << "\"ts\":" << event.ts;
   writer_ << "},";
   writer_ << "\n";
 
   writer_.flush();
 }
 
+void TimeLine::writer_do_work() {
+  // init file
+  writer_.open(timeline_path_, std::ios::out);
+
+  ARGUMENT_CHECK(writer_.is_open(), "open file:" << timeline_path_ << " failed!");
+
+  writer_ << "[\n";
+  writer_.flush();
+
+  initialized_ = true;
+
+  while (true) {
+    TimeEvent event;
+
+    if (time_event_queue_.try_dequeue(event)) {
+      write(event);
+    } else if (stoped_) {
+      break;
+    } else {
+      std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+  }
+
+  writer_.close();
+}
+
 // event begin
 // sormat: {"name": "Asub1", "cat": "PERF", "ph": "B", "pid": "event", "ts": 829},
 void TimeLine::begin(const std::string &op_name, const std::string& event_name) {
-  std::unique_lock<std::mutex> lock(mutex_);
+  TimeEvent event = {
+    event_name,
+    std::string("PERF"),
+    std::string("B"),
+    op_name,
+    get_current_microseconds()
+  };
 
-  write(event_name, std::string("PERF"), std::string("B"), op_name, get_current_microseconds());
+  time_event_queue_.enqueue(std::move(event));
 }
 
 // event end
 void TimeLine::end(const std::string &op_name, const std::string& event_name) {
-  std::unique_lock<std::mutex> lock(mutex_);
+  TimeEvent event = {
+    event_name,
+    std::string("PERF"),
+    std::string("E"),
+    op_name,
+    get_current_microseconds()
+  };
 
-  write(event_name, std::string("PERF"), std::string("E"), op_name, get_current_microseconds());
+  time_event_queue_.enqueue(std::move(event));
 }
 
 // a lot of event begin
 void TimeLine::begin(const std::vector<std::string> &op_names, const std::string& event_name) {
-  std::unique_lock<std::mutex> lock(mutex_);
-
   std::string cat("PERF");
   std::string ph("B");
 
   auto ts = get_current_microseconds();
 
   for (auto &op_name : op_names) {
-    write(event_name, cat, ph, op_name, ts);
+    TimeEvent event = {
+      event_name,
+      cat,
+      ph,
+      op_name,
+      ts
+    };
+
+    time_event_queue_.enqueue(std::move(event));
   }
 }
 
 // a lot of event end
 void TimeLine::end(const std::vector<std::string> &op_names, const std::string& event_name) {
-  std::unique_lock<std::mutex> lock(mutex_);
-
   std::string cat("PERF");
   std::string ph("E");
 
   auto ts = get_current_microseconds();
 
   for (auto &op_name : op_names) {
-    write(event_name, cat, ph, op_name, ts);
+    TimeEvent event = {
+      event_name,
+      cat,
+      ph,
+      op_name,
+      ts
+    };
+
+    time_event_queue_.enqueue(std::move(event));
   }
 }
 
 // a lot of task begin
 void TimeLine::begin(const std::vector<Task> &tasks, const std::string& event_name) {
-  std::unique_lock<std::mutex> lock(mutex_);
-
   std::string cat("PERF");
   std::string ph("B");
 
   auto ts = get_current_microseconds();
 
   for (auto &task : tasks) {
-    write(event_name, cat, ph, task.name, ts);
+    TimeEvent event = {
+      event_name,
+      cat,
+      ph,
+      task.name,
+      ts
+    };
+
+    time_event_queue_.enqueue(std::move(event));
   }
 }
 
 //a lot of task end
 void TimeLine::end(const std::vector<Task> &tasks, const std::string& event_name) {
-  std::unique_lock<std::mutex> lock(mutex_);
-
   std::string cat("PERF");
   std::string ph("E");
 
   auto ts = get_current_microseconds();
 
   for (auto &task : tasks) {
-    write(event_name, cat, ph, task.name, ts);
+    TimeEvent event = {
+      event_name,
+      cat,
+      ph,
+      task.name,
+      ts
+    };
+
+    time_event_queue_.enqueue(std::move(event));
   }
 }
 
 void TimeLine::begin(const std::vector<TaskKey> &task_keys, const std::string& event_name) {
-  std::unique_lock<std::mutex> lock(mutex_);
-
   std::string cat("PERF");
   std::string ph("B");
 
   auto ts = get_current_microseconds();
 
   for (auto &key : task_keys) {
-    write(event_name, cat, ph, std::get<1>(key), ts);
+    TimeEvent event = {
+      event_name,
+      cat,
+      ph,
+      std::get<1>(key),
+      ts
+    };
+
+    time_event_queue_.enqueue(std::move(event));
   }
 }
 
 void TimeLine::end(const std::vector<TaskKey> &task_keys, const std::string& event_name) {
-  std::unique_lock<std::mutex> lock(mutex_);
-
   std::string cat("PERF");
   std::string ph("E");
 
   auto ts = get_current_microseconds();
 
     for (auto &key : task_keys) {
-    write(event_name, cat, ph, std::get<1>(key), ts);
+      TimeEvent event = {
+      event_name,
+      cat,
+      ph,
+      std::get<1>(key),
+      ts
+    };
+
+    time_event_queue_.enqueue(std::move(event));
   }
 }
 
--- dadt/internal/timeline.h
+++ dadt/internal/timeline.h
@@ -8,6 +8,8 @@
 #include <chrono>
 #include <vector>
 
+#include "concurrentqueue.h"
+
 #include "definition.h"
 #include "task.h"
 
@@ -25,10 +27,17 @@ const std::string kDoBroadCastEvent = "DoBroadCast";
 // s special event indicate in training but not cccurate
 const std::string kInTrainingEvent = "InTraining";
 
+struct TimeEvent {
+  std::string name;
+  std::string cat;
+  std::string ph;
+  std::string pid;
+  int64_t ts;
+};
+
 // a simple time line 
 // use chrome://tracing to open it
 class TimeLine {
-
 private:
   // timeline path
   std::string timeline_path_;
@@ -36,18 +45,24 @@ private:
   // file stream
   std::ofstream writer_;
 
-  // a mutex
-  std::mutex mutex_;
+  // use a lock free queue to store time event
+  moodycamel::ConcurrentQueue<TimeEvent> time_event_queue_;
+
+  // use a background thread to write event to file
+  std::thread writer_thread_;
+
+  std::atomic<bool> initialized_;
+  std::atomic<bool> stoped_;
 
 private:
   // get current microseconds
   int64_t get_current_microseconds();
 
-  void write(const std::string &name, 
-             const std::string &cat, 
-             const std::string &ph, 
-             const std::string &pid, 
-             int64_t ts);
+  void write(const TimeEvent &event);
+
+  // iterator get time event and write to file
+  void writer_do_work();
+
 public:
   TimeLine(const std::string &timeline_path);
 
